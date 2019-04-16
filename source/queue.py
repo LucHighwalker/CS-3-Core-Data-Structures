3b434dfbc4657c560986614e98b581d570331b7e
LucHighwalker <luc.highwalker@gmail.com>
2019-04-15 22:29:24 -0700

    linked queue

--- source/queue.py
+++ source/queue.py
@@ -22,26 +22,41 @@ class LinkedQueue(object):
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise."""
         # TODO: Check if empty
+        return self.list.is_empty()
 
     def length(self):
         """Return the number of items in this queue."""
         # TODO: Count number of items
+        return self.list.length()
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue.
-        Running time: O(???) – Why? [TODO]"""
+        Running time: O(1) – Because it just calls the append methos, which thanks
+        to the tail, is constant time"""
         # TODO: Insert given item
+        self.list.append(item)
 
     def front(self):
         """Return the item at the front of this queue without removing it,
         or None if this queue is empty."""
         # TODO: Return front item, if any
+        if self.is_empty():
+            return None
+        else:
+            return self.list.head.data
 
     def dequeue(self):
         """Remove and return the item at the front of this queue,
         or raise ValueError if this queue is empty.
-        Running time: O(???) – Why? [TODO]"""
+        Running time: O(1) – Because deleting the head node doesn't require going through
+        the entire linked list."""
         # TODO: Remove and return front item, if any
+        if self.is_empty():
+            raise ValueError("Queue is empty.")
+        else:
+            data = self.list.head.data
+            self.list.delete(data)
+            return data
 
 
 # Implement ArrayQueue below, then change the assignment at the bottom
